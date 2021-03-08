d8a4531ef22d6a09065fad5431938c1645265bef
eric satoshi <satoshi.eric@gmail.com>
2021-03-08 15:52:22 -0300

    Módulo "Rotas" adicionado

---
+++ Rotas/rotas.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const app = express()
+
+app.get("/", function(req, res) {
+    res.send("Seja bem vindo ao meu app")
+})
+
+app.get("/sobre", function(req, res) {
+    res.send("Minha página sobre")
+})
+
+app.get("/blog", function(req, res) {
+    res.send("Bem vindo ao meu blog")
+})
+
+app.listen(8081, function() {
+    console.log("Servidor rodando na url http://localhost:8081")
+})
