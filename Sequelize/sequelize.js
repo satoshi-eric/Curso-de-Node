c6355ac2e60387df554a5d0884586a1dc160800a
eric satoshi <satoshi.eric@gmail.com>
2021-03-08 16:41:45 -0300

    Módulo "Sequelize" adicionado

--- Sequelize/sequelize.js
+++ Sequelize/sequelize.js
@@ -14,3 +14,43 @@ sequelize.authenticate().then(function() {
 }).catch(function(erro) {
     console.log("Falha ao se conectar: " + erro)
 })
+
+const Postagem = sequelize.define('postagens', {
+    titulo: {
+        type: Sequelize.STRING
+    },
+    conteudo: {
+        type: Sequelize.TEXT
+    }
+})
+
+Postagem.sync({force: true})
+
+Postagem.create({
+	titulo: "UM TÍTULO QUALQUER",
+	conteudo: "jkldasjfdsafkjaskfçjafljç fjdaskljfkadsçfjkçfakfsj"
+})
+
+const Usuario = sequelize.define('usuarios', {
+    nome: {
+        type: Sequelize.STRING
+    },
+    sobrenome: {
+        type: Sequelize.STRING
+    },
+    idade: {
+        type: Sequelize.INTEGER
+    },
+    email: {
+        type: Sequelize.STRING
+    }
+})
+
+Usuario.sync({force: true})
+
+Usuario.create({
+    nome: "Eric",
+    sobrenome: "Satoshi",
+    idade: 20,
+    email: "eric@gmail.com"
+})
