b00ea716d44d66375a2387b05ce08281940296ef
eric satoshi <satoshi.eric@gmail.com>
2021-03-08 16:40:14 -0300

    Módulo "Sequelize" adicionado

---
+++ Sequelize/sequelize.js
@@ -0,0 +1,16 @@
+const Sequelize = require('sequelize')
+const sequelize = new Sequelize(
+    'test', 
+    'root', 
+    'toor', 
+    {
+        host: 'localhost', 
+        dialect: 'mysql'
+    }
+)
+
+sequelize.authenticate().then(function() {
+    console.log("Conectado com sucesso!")
+}).catch(function(erro) {
+    console.log("Falha ao se conectar: " + erro)
+})
